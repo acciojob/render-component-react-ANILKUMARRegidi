47f899454966a4d5df953691f621e1d69dc633f1
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2024-03-24 11:19:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import '../styles/App.css';
+class App extends component
+  {
+render() {
+        return (
+            <div className="grt">
+                 <p> "I am learning React. My life is getting better."</p>
+            </div>
+        );
+    }
 
-const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
-
-
+  }
 export default App;
